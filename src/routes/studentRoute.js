3d8a0eeda74f46ae9a8d14938a7e8b4adf9e6a2c
shivamu1818 <shivamuoadhyay4@gmail.com>
2026-05-15 20:52:46 +0530

    adding initial template

---
+++ src/routes/studentRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+// const commonController = require("../controllers/commonController");
+// const validateUserToken = require("../middleware/validateUserMiddleware");
+
+// router.get("/common/all-users", validateUserToken, commonController.getUsers);
+// router.get("/messages/:userId", validateUserToken, commonController.getMessages);
+
+module.exports = router;
