33dde3c195427e6c6886f2c85d70bbbc776bded6
shivamu1818 <shivamuoadhyay4@gmail.com>
2026-07-24 19:39:16 +0530

    fixing adding teacher

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,17 +1,22 @@
-const jwt = require('jsonwebtoken');
-const {verifyToken}=require("../utils/jwt")
+const { verifyAccessToken } = require('../utils/jwt');
+
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(' ')[1];
+    const bearerToken = req.headers.authorization?.startsWith('Bearer ')
+      ? req.headers.authorization.split(' ')[1]
+      : null;
 
+    const cookieToken = req.cookies?.edu_excel_acc_token;
+
+    const token = bearerToken || cookieToken;
     if (!token) {
       return res.status(401).json({
         message: 'Unauthorized',
       });
     }
 
-    const decoded = verifyToken(token);
-    req.user = decoded;
+    req.user = verifyAccessToken(token);
+
     next();
   } catch (err) {
     return res.status(401).json({
